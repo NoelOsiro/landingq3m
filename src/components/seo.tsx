2a9663f6a161890a85339e81c8bfc00388ad949d
NoelOsiro <noel.osiro@azubiafrica.org>
2024-10-12 02:03:05 +0300

    seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -8,19 +8,20 @@ interface SEOProps {
 }
 
 export default function SEO({ title, description, image, url }: SEOProps) {
+  const defaultImage = '../assets/images/logo.png'
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={image || defaultImage} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={image || defaultImage} />
     </Head>
   )
 }
